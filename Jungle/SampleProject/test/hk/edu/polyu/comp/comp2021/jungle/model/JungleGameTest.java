c4cdd93a6d184f9379cc29a5bb2b259b758e145f
Hormi Sami <29134089+samihormi@users.noreply.github.com>
2019-10-29 20:18:10 +0800

    Sample Project/Instructions

---
+++ Jungle/SampleProject/test/hk/edu/polyu/comp/comp2021/jungle/model/JungleGameTest.java
@@ -0,0 +1,19 @@
+package hk.edu.polyu.comp.comp2021.jungle.model;
+
+import hk.edu.polyu.comp.comp2021.jungle.model.JungleGame;
+import org.junit.Test;
+
+
+public class JungleGameTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testJungleGameConstructor(){
+        JungleGame game = new JungleGame();
+        assert true;
+    }
+
+}
