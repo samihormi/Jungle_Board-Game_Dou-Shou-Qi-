85363e88373333e246245a6712fe31ceeb31c621
German <18085407d@connect.polyu.hk>
2019-11-08 14:30:28 +0800

    Added BoardController

---
+++ Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/controller/BoardController.java
@@ -0,0 +1,9 @@
+package hk.edu.polyu.comp.comp2021.jungle.controller;
+import hk.edu.polyu.comp.comp2021.jungle.model.Position;
+
+public class BoardController {
+    private Position moveDestination;
+    //isValidMove(){
+
+    //}
+}
