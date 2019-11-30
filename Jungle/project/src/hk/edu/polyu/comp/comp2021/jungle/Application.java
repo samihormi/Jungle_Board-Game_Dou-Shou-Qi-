d4be1680eebe7d525c312662a6557550d3307388
German <18085407d@connect.polyu.hk>
2019-11-30 17:19:04 +0300

    Added basic GUI with colors and layout

--- Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/Application.java
+++ Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/Application.java
@@ -3,6 +3,7 @@ package hk.edu.polyu.comp.comp2021.jungle;
 import java.util.*;
 import hk.edu.polyu.comp.comp2021.jungle.controller.*;
 import hk.edu.polyu.comp.comp2021.jungle.model.*;
+import hk.edu.polyu.comp.comp2021.jungle.gui.*;
 
 public class Application {
     public static void main(String[] args) throws NullPointerException{
@@ -11,7 +12,7 @@ public class Application {
         String playerName2 = input.next();
         Board board = new Board(playerName1,playerName2);
         // start playing the game
-
+        Table table = new Table(board);
         //GameController gc = new GameController();
         //BoardController bc = new BoardController();
     }
