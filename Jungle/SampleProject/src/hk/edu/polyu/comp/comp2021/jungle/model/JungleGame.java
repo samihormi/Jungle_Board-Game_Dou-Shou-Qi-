6c949ebf22bc18efdd32ac5f3a32f1ffdc2234f1
changwoochoi2 <changwoochoi2@users.noreply.github.com>
2019-12-01 13:20:44 +0800

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 1c36832d90346ea183734b31b0e52a1ce467d794.

--- Jungle/SampleProject/src/hk/edu/polyu/comp/comp2021/jungle/model/JungleGame.java
+++
@@ -1,7 +0,0 @@
-package hk.edu.polyu.comp.comp2021.jungle.model;
-
-public class JungleGame {
-
-    public JungleGame(){}
-
-}
--- Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/Application.java
+++ Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/Application.java
@@ -1,11 +1,19 @@
 package hk.edu.polyu.comp.comp2021.jungle;
 
-import hk.edu.polyu.comp.comp2021.jungle.controller.GameController;
+import java.util.*;
+import hk.edu.polyu.comp.comp2021.jungle.controller.*;
+import hk.edu.polyu.comp.comp2021.jungle.model.*;
+import hk.edu.polyu.comp.comp2021.jungle.gui.*;
 
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) throws NullPointerException{
+        Scanner input = new Scanner(System.in);
+        String playerName1 = input.next();
+        String playerName2 = input.next();
+        Board board = new Board(playerName1,playerName2);
         // start playing the game
-        GameController gc = new GameController();
-
+        Table table = new Table(board);
+        //GameController gc = new GameController();
+        //BoardController bc = new BoardController();
     }
 }
