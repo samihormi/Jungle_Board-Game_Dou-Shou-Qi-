c9da206ab35c6bc121cd23317c0a7efb372e1d56
Park-junhong <40653860+Park-junhong@users.noreply.github.com>
2019-12-01 13:20:33 +0900

    [just for test].

--- Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/controller/BoardController.java
+++ Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/controller/BoardController.java
@@ -5,32 +5,44 @@ import hk.edu.polyu.comp.comp2021.jungle.model.Board;
 import hk.edu.polyu.comp.comp2021.jungle.model.Player;
 import hk.edu.polyu.comp.comp2021.jungle.model.Position;
 
-public class BoardController {
-    public int moveAnimal(Position p1, Position p2, Board board){ //turn will consider the player. we don't have to deal with that here
+public class BoardController {//0=plain, 1=river, 2=trap, 3=goal(w
+    public void moveAnimal(Position p1, Position p2, Board board){ //turn will consider the player. we don't have to deal with that here
         //isvalidmove() but there is exception
         Block[][] blocks = board.getBoard();
-        if(Math.pow(p1.getX()-p2.getX(),2)+Math.pow(p1.getY()-p2.getY(),2)!=1)
+        if(Math.pow(p1.getX()-p2.getX(),2)+Math.pow(p1.getY()-p2.getY(),2)==1) //check distance = 1
         {
-            System.out.println("you cannot move more than one block");
-            return -1;//fail
+            if(blocks[p2.getX()][p2.getY()].getBlockType()!=1){//not river
+                if(blocks[p2.getX()][p2.getY()].getA()!=null){ //if there is any enemy
+
+                }
+                else{ //empty
+                    move(p1,p2,board);
+                }
+            }
+            else{//river
+
+            }
         }
         if(blocks[p2.getX()][p2.getY()].getBlockType()==1){ //if it is river
             System.out.println("you cannot move to river");
-            return -1;//fail
-        }
-        if(blocks[p2.getX()][p2.getY()].getA()!=null){ //if there is any enemy
-            if(blocks[p1.getX()][p1.getY()].getA().getRank()<blocks[p2.getX()][p2.getY()].getA().getRank()){
-                if(blocks[p1.getX()][p1.getY()].getA().getRank()!=0){
-                    System.out.println("you cannot kill higher rank animal");
-                    return -1;//fail
+            if(blocks[p2.getX()][p2.getY()].getA()!=null){ //if there is any enemy
+                if(blocks[p1.getX()][p1.getY()].getA().getRank()<blocks[p2.getX()][p2.getY()].getA().getRank()){
+                    if(blocks[p1.getX()][p1.getY()].getA().getRank()!=0){
+                        System.out.println("you cannot kill higher rank animal");
+                    }
                 }
             }
         }
-        return 0;//success
+
+
 
     }
-    public boolean canEat(){
-        return false;
+
+    public void move(Position p1, Position p2, Board board){
+
+    }
+    public void Eat(){
+
     }
     public int doMove(Position p1,Position p2){
         return -1;
