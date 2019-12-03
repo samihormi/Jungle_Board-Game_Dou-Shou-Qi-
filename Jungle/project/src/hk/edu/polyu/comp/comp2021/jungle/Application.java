a92bf4e9a1d2f7f4b6dcfeebcec193cebc41af02
sharrik21 <57133844+sharrik21@users.noreply.github.com>
2019-12-03 15:07:14 +0300

    Update Application.java
    Changed SBoard and NBoard

--- Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/Application.java
+++ Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/Application.java
@@ -6,14 +6,29 @@ import hk.edu.polyu.comp.comp2021.jungle.model.*;
 import hk.edu.polyu.comp.comp2021.jungle.gui.*;
 
 public class Application {
-    public static void main(String[] args) throws NullPointerException{
+    public static void main(String[] args) throws NullPointerException, InterruptedException{
         Scanner input = new Scanner(System.in);
-        String playerName1 = input.next();
-        String playerName2 = input.next();
-      // Board board = new Board(playerName1,playerName2);
-        SBoard sbord = new SBoard();
+        Player player1 = new Player("", 1),
+                player2 = new Player("", 2);
+        SBoard sBoard = new SBoard();
+        while(!sBoard.isNewGame && !sBoard.isLoadGame){
+            Thread.sleep(200);
+        }
+        NBoard nBoard;
+        if(sBoard.isNewGame){
+            nBoard = new NBoard();
+            while(!nBoard.isGameStarted){
+                Thread.sleep(200);
+            }
+            player1.setName(nBoard.getName1());
+            player2.setName(nBoard.getName2());
+        }
+        else{
+            System.out.println("Here you load...");
+        }
+        Board board = new Board(player1,player2);
         // start playing the game
-        //Table table = new Table(sboard);
+        Table table = new Table(board);
         //GameController gc = new GameController();
         //BoardController bc = new BoardController();
     }
