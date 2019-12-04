3da3f8e9580314850f99dad7954d55ba0312e72c
Park-junhong <40653860+Park-junhong@users.noreply.github.com>
2019-12-05 01:32:26 +0900

    Merge branch 'master' of /Users/parkjunhong/Documents/GitHub/Chinese-Board-Game with conflicts.

--- Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/controller/GameController.java
+++ Jungle/project/src/hk/edu/polyu/comp/comp2021/jungle/controller/GameController.java
@@ -2,6 +2,7 @@ package hk.edu.polyu.comp.comp2021.jungle.controller;
 
 import hk.edu.polyu.comp.comp2021.jungle.Save.ResourceManager;
 import hk.edu.polyu.comp.comp2021.jungle.Save.SaveGame;
+import hk.edu.polyu.comp.comp2021.jungle.gui.Table;
 import hk.edu.polyu.comp.comp2021.jungle.model.*;
 
 public class GameController {
@@ -11,7 +12,10 @@ public class GameController {
     public Board board;
 
     public GameController(String player1_name,String player2_name) {
-        StartGame(player1_name,player2_name);//we have to get player name
+        try {
+            StartGame(player1_name,player2_name);
+        }catch (InterruptedException e){}
+
     }
     public GameController(Board board,Player player1,Player player2, Player turn) {
         this.board = board;
@@ -21,7 +25,7 @@ public class GameController {
         SaveGame s1 = new SaveGame(board,player1,player2,turn);
         saveGame(s1);
     }
-    public void StartGame(String player1_name,String player2_name){
+    public void StartGame(String player1_name,String player2_name) throws InterruptedException{
 
         player1 = new Player(player1_name, 1);
         player2 = new Player(player2_name, 2);
@@ -29,16 +33,20 @@ public class GameController {
         board = new Board(player1,player2);
 
         BoardController boardController = new BoardController(board);
-        /*
+        Table table =new Table(board,player1,player2,player1);
+        Position p[]=null;
         while(boardController.isEnd()){
-            //Position[] p = getInputFromTable(player1); // p[0] = current location p[1]=destination
-           // if(boardController.moveRules(p[0],p[1])==1) // valid move = 1, invalid move = 0
+            while (!table.ready) {
+                p=table.getInputFromTable(turn); // p[0] = current location p[1]=destination
+                Thread.sleep(200);
+            }
+            if(boardController.moveRules(p[0],p[1])==1) // valid move = 1, invalid move = 0
             {
                 turn = chageTurn(player1, player2,turn);
-             //   updateTable(board);
+                table.updateTable(board);
             }
-
-        }*/
+            table.setReady(false);
+        }
 
     }
     public void saveGame(SaveGame s1){
