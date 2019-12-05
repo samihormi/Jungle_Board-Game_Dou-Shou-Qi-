77ebc6fe43c98294692354cac5241d900c33991e
sharrik21 <57133844+sharrik21@users.noreply.github.com>
2019-12-05 12:50:25 +0300

    Javadoc

--- Jungle/project/test/hk/edu/polyu/comp/comp2021/jungle/model/PositionTest.java
+++ Jungle/project/test/hk/edu/polyu/comp/comp2021/jungle/model/PositionTest.java
@@ -3,7 +3,13 @@ package hk.edu.polyu.comp.comp2021.jungle.model;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+     * Position Class Test
+     */
 public class PositionTest {
+    /**
+     * Full Test
+     */ 
     @Test
     public void positionTest() throws Exception{
         Position actual = new Position(5, 1);
